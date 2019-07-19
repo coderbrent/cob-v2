3313c5f0b1b925f75ff46021931a28aad33e5be3
Brent Abruzese <brent.abruzese@gmail.com>
2019-07-19 15:31:00 -0400

    added root route to test server

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('server is working...')
+})
+
 app.listen(port, () => console.log(`app is now listening on port ${port}`))
